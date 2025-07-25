e1d4a0eaf5c490083fc0edb1ba0ca7ef88f9b735
릴리맥Nc 이스트폴 <lilymagnc@gmail.com>
2025-07-25 22:50:22 +0000

    === Deploying to 'lilymagerp-fs1'...
    i  deploying functions, hosting
    i

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 
 import { https } from 'firebase-functions';
 import { default as next } from 'next';
+import { defineString } from 'firebase-functions/params';
+
+// Define parameters for Firebase app config
+defineString('NEXT_PUBLIC_FIREBASE_API_KEY');
+defineString('NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN');
+defineString('NEXT_PUBLIC_FIREBASE_PROJECT_ID');
+defineString('NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET');
+defineString('NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID');
+defineString('NEXT_PUBLIC_FIREBASE_APP_ID');
 
 const nextServer = next({
   dev: false,
