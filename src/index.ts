f4920bb4ff79fd3a65a1661d5ee271e16efe4123
릴리맥Nc 이스트폴 <lilymagnc@gmail.com>
2025-07-25 21:25:26 +0000

    오류를 확인하고 수정해줘

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@
 import { https } from 'firebase-functions';
 import { default as next } from 'next';
 
-const server = next({
+const nextServer = next({
   dev: false,
   conf: {
     distDir: '.next',
   },
 });
 
-const nextjsHandle = server.getRequestHandler();
+const nextjsHandle = nextServer.getRequestHandler();
 
 export const server = https.onRequest((req, res) => {
-  return server.prepare().then(() => nextjsHandle(req, res));
+  return nextServer.prepare().then(() => nextjsHandle(req, res));
 });
