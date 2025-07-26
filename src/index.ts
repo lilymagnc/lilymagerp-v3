cbcc853d7590d3485b556c21305ab39e6d0ccfe0
릴리맥Nc 이스트폴 <lilymagnc@gmail.com>
2025-07-26 00:43:59 +0000

    왜 자꾸 같은 에러가 반복이 될까?> ○  (Static)   prerendered as static content ƒ  (Dy

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 
-import {https, setGlobalOptions} from 'firebase-functions/v2';
+import { onRequest } from "firebase-functions/v2/https";
+import { setGlobalOptions } from "firebase-functions/v2";
 import next from 'next';
 import path from 'path';
 
 // 2세대 함수를 위한 전역 옵션 설정
-setGlobalOptions({maxInstances: 10, region: 'us-central1'});
+setGlobalOptions({ maxInstances: 10, region: 'us-central1' });
 
 const nextjsDistDir = path.join(
   path.dirname(new URL(import.meta.url).pathname),
@@ -20,6 +21,6 @@ const nextServer = next({
 
 const nextjsHandle = nextServer.getRequestHandler();
 
-export const server = https.onRequest((req, res) => {
+export const server = onRequest((req, res) => {
   return nextServer.prepare().then(() => nextjsHandle(req, res));
 });
