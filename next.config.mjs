6c7513a5a783614dab9a299d2d710231ec89e826
릴리맥Nc 이스트폴 <lilymagnc@gmail.com>
2025-07-26 10:31:23 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,29 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'ecimg.cafe24img.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ecimg.cafe24img.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
+
 export default nextConfig;
